22e03fa887e44b4af17bb15bd4eea3c2d6c2b299
JyothiSweashika <jyothisweashikaraavi@gmail.com>
2023-07-19 15:59:15 +0530

    assignment5

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string validUsername = "john";
+            string validPassword = "pass123";
+            int attempts = 0;
+
+            while (attempts < 3)
+            {
+                Console.Write("Enter username: ");
+                string username = Console.ReadLine();
+
+                Console.Write("Enter password: ");
+                string password = Console.ReadLine();
+
+                if (username == validUsername && password == validPassword)
+                {
+                    Console.WriteLine("Login successful.");
+                    break;
+                }
+                else if (username == validUsername)
+                {
+                    Console.WriteLine("Invalid username.");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid password.");
+                }
+
+                attempts++;
+            }
+
+            if (attempts == 3)
+            {
+                Console.WriteLine("Login failed");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
